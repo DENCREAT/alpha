f61a3af1d3273591ce54e75a907a5083055d31d9
Siarhei <s.kamianiok@sam-solutions.com>
2018-11-09 16:20:22 +0300

    about slider implementation

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,26 @@
         variableWidth: true
     });
 })();
+
+(function() {
+    const $aboutSliderLink = $('.js-about-slider-item-link');
+    const $aboutSlider = $('.js-about-slider');
+    const $aboutSliderPrev = $('.js-about-slider-prev');
+    const $aboutSliderNext = $('.js-about-slider-next');
+
+    console.log($aboutSliderLink);
+
+    $aboutSlider.on('init', function(event, slick){
+        slick.$slider.addClass('initialized');
+    });
+
+    $aboutSliderLink.on('click', function(event) {
+        event.preventDefault();
+    });
+
+    $aboutSlider.slick({
+        prevArrow: $aboutSliderPrev,
+        nextArrow: $aboutSliderNext,
+        slidesToShow: 1
+    });
+})();
